578d4858be88c6b83862b4c0b7a7f25496d5e7d5
juhkamsoo <hendrik.juhkamsoo@tptlive.ee>
2022-03-22 09:45:23 +0200

    Create 1-tiktak.py

---
+++ 1-tiktak.py
@@ -0,0 +1,51 @@
+print("Game started")
+print("1 2 3")
+print("4 5 6")
+print("7 8 9")
+
+ruut = input("P1, Sisesta ruut nr:")
+print(ruut)
+if ruut == "1":
+    print("x 2 3")
+    print("4 5 6")
+    print("7 8 9")
+
+if ruut == "2":
+    print("1 x 3")
+    print("4 5 6")
+    print("7 8 9")
+
+if ruut == "3":
+    print("1 2 x")
+    print("4 5 6")
+    print("7 8 9")
+
+if ruut == "4":
+    print("1 2 3")
+    print("x 5 6")
+    print("7 8 9")
+
+if ruut == "5":
+    print("1 2 3")
+    print("4 x 6")
+    print("7 8 9")
+
+if ruut == "6":
+    print("1 2 3")
+    print("4 5 x")
+    print("7 8 9")
+
+if ruut == "7":
+    print("1 2 3")
+    print("4 5 6")
+    print("x 8 9")
+
+if ruut == "8":
+    print("1 2 3")
+    print("4 5 6")
+    print("7 x 9")
+
+if ruut == "9":
+    print("1 2 3")
+    print("4 5 6")
+    print("7 8 x")
